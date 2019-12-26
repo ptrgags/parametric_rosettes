999877418ff57b6596393af56ac44fad4f819d36
Peter Gagliardi <ptrgags@gmail.com>
2019-12-26 09:17:31 -0500

    Add half the patterns

--- FourierSeries.js
+++ FourierSeries.js
@@ -1,12 +1,26 @@
+/**
+ * This class represents a finite sum of circular motions of the form
+ *
+ * f(t) = sum_k a_k e^(i n_k t)
+ *
+ * where a_k = amplitude * e^(i phase) is a complex number
+ *       n_k is an integer
+ *       t is a real number, typically in the range [0, 2 pi]
+ */
 class FourierSeries {
     /**
      * terms - an array of triples [frequency, amplitude, phase]
-     * The frequencies must be listed in ascending order
+     * the terms must be specified in ascending order so the
+     * partial sums are predictable.
      */
     constructor(terms) {
         this.terms = terms;
     }
     
+    /**
+     * To plot the drawing arm, we need not just the total sum,
+     * but all the partial sums.
+     */
     * partial_sums(t) {
         let x = 0.0;
         let y = 0.0;
@@ -19,16 +33,42 @@ class FourierSeries {
             yield [x, y,];
         }
     }
-    
+ 
+    /**
+     * For convenience, this gets the final sum from this.partial_sums()
+     */
     position(t) {
         const sums = this.partial_sums(t);
         return sums[sums.length - 1];
     }
     
+    /**
+     * Return a new curve scaled by a real number.
+     * this is done by scaling the amplitudes
+     */
+    scale(amount) {
+        return new FourierSeries(
+            this.terms.map(([freq, amp, phase]) => [freq, amp * amount, phase,]));
+    }
+    
+    /**
+     * Rotate a curve by an angle in radians
+     * This is done by adding the angle to the phase of all terms.
+     */
+    rotate(angle) {
+        return new FourierSeries(
+            this.terms.map(([freq, amp, phase]) => [freq, amp, phase - angle,]));
+    }
+ 
+    /**
+     * Shorthand string for representing a curve in the form
+     * freq,amp,phase:freq,amp,phase:...
+     */
     to_string() {
         let terms = [];
         for (const [frequency, amplitude, phase] of this.terms) {
-            term_str = `${frequency},${amplitude},${phase}`;
+            const term_str = `${frequency},${amplitude},${phase}`;
+            terms.push(term_str);
         }
         return terms.join(':');
     }
--- parametric_rosettes.js
+++ parametric_rosettes.js
@@ -1,10 +1,30 @@
 const MAX_X = 2.0;
 const PERIOD = 800;
-let pattern = ROSETTE_8K3;
+const THICKNESS = 3.0;
+let pattern = ROSETTES["8k + 3"];
 let curve = [];
+let start_frame = 0;
+
+function make_select() {
+    const sel = createSelect();
+    sel.position(10, 10);
+    for (const x of Object.keys(ROSETTES)) {
+        sel.option(x);
+    }
+    sel.changed(change_rosette);
+}
+
+function change_rosette(e) {
+    const selected = e.target.value;
+    pattern = ROSETTES[selected];
+    curve = [];
+    start_frame = frameCount;
+}
 
 function setup() {
     createCanvas(500, 750);
+    make_select();
+    
     background(0);
     console.log(width);
 }
@@ -22,22 +42,26 @@ function draw_polyline(points, close) {
 }
 
 function draw() {
-    const t = (frameCount / PERIOD) * TWO_PI;
+    const frame = frameCount - start_frame;
+    const t = (frame / PERIOD) * TWO_PI;
     background(0);
     
     push();
     translate(width / 2, height / 2);
     const scale_factor = width / 2 / MAX_X;
     scale(scale_factor, -scale_factor);
-    strokeWeight(1.0 / scale_factor);
+    strokeWeight(THICKNESS / scale_factor);
     
     const sums = [...pattern.partial_sums(t),];
-    if (frameCount < PERIOD) {
+    if (frame < PERIOD) {
         curve.push(sums[sums.length - 1]);
     }
+    
+    noFill();
+    strokeJoin(ROUND);
+    stroke(71, 142, 204);
+    draw_polyline(curve, frame >= PERIOD);
     stroke(255);
-    noFill(); 
-    draw_polyline(curve, frameCount >= PERIOD);
     draw_polyline(sums, false);
     
     pop();
